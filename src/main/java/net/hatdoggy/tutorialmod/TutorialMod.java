b55117c9b8564600986a1b53fe658d36d871e926
Kristopher Santos <topers1820@gmail.com>
2022-12-06 23:04:56 +0800

    Testing comment of "Hatdugan Style"

--- src/main/java/net/hatdoggy/tutorialmod/TutorialMod.java
+++ src/main/java/net/hatdoggy/tutorialmod/TutorialMod.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+//Hatdugan style
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
